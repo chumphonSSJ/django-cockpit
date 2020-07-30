5e13aa2f8e8294077a3f297d29ee038876efc836
joblackpoc <duopoc@gmail.com>
2020-07-30 14:42:12 +0700

    Rebuild Cockpit r11

---
+++ Cockpit/main/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from main.models import *
+
+# Register your models here.
+admin.site.register(Ssj)
+admin.site.register(Reponse_kpi)
+admin.site.register(Kpi)
+admin.site.register(Profile)
+admin.site.register(KeyInput)
+admin.site.register(Person)
+admin.site.register(Country)
+admin.site.register(City)
+admin.site.register(Chospcode)
+admin.site.register(Dep)
+admin.site.register(Group)
+admin.site.register(Kpi_eval_prov)
+admin.site.register(Kpi_eval_rh)
+admin.site.register(Kpi_hosp)
+admin.site.register(Kpi_index)
+admin.site.register(Kpi_input)
+admin.site.register(Kpi_prov)
+admin.site.register(Pk_byear)
+admin.site.register(Task)
+admin.site.register(Trole)
+    
